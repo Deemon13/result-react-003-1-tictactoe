c72b4eb24631e563d4d5853a06a956fac7d08a01
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-03-24 21:30:40 +0300

    added AI - 0

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -11,16 +11,20 @@ export const Game = () => {
 	const [isDraw, setIsDraw] = useState(false);
 	const [field, setField] = useState(fieldArr);
 
+	// const [freeCells, setFreeCells] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+
+	const newFields = field.slice();
+
 	function handleClickOnFieldCell(id) {
 		if (isGameEnded) {
 			return null;
 		}
 
-		const newFields = field.slice();
-
 		if (field[id] === '' && !isGameEnded) {
 			newFields[id] = currentPlayer;
 			setField(newFields);
+			// const newFreeCells = getFreeCells(freeCells, id);
+			// setFreeCells(newFreeCells);
 		} else {
 			return null;
 		}
@@ -36,6 +40,24 @@ export const Game = () => {
 	function setWinner(fieldCells, player) {
 		return WIN_PATTERNS.some(el => el.every(item => fieldCells[item] === player));
 	}
+
+	// function getFreeCells(arr, idx) {
+	// 	const newArr = arr.filter(item => item !== idx);
+	// 	return newArr;
+	// }
+
+	// function getRandomNumber(min, max) {
+	// 	return Math.round(Math.random() * (max - min) + min);
+	// }
+
+	// if (currentPlayer === '0') {
+	// 	const freeCell = freeCells[getRandomNumber(0, freeCells.length - 1)];
+	// 	newFields[freeCell] = currentPlayer;
+	// 	setField(newFields);
+	// 	const newFreeCells = getFreeCells(freeCells, freeCell);
+	// 	setFreeCells(newFreeCells);
+	// 	setCurrentPlayer(prevState => (prevState === 'X' ? '0' : 'X'));
+	// }
 
 	if (field.every(cell => cell !== '') && !isDraw && !isGameEnded) {
 		setIsDraw(true);
@@ -55,6 +77,7 @@ export const Game = () => {
 		setIsDraw(false);
 		setIsGameEnded(false);
 		setField([...fieldArr]);
+		// setFreeCells([0, 1, 2, 3, 4, 5, 6, 7, 8]);
 	}
 
 	return (
